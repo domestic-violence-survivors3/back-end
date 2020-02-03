a7cd3df9faefdd3aa57a8322c3b358c98a510308
Shane Jeremich <shane.jeremich@gmail.com>
2020-02-03 13:39:39 -0700

    first commit

---
+++ knexfile.js
@@ -0,0 +1,42 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./database/dvs.db3" // *** UPDATE *** database name
+    },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    },
+    migrations: {
+      directory: "./database/migrations",
+      tableName: "dbmigrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    }
+  },
+  testing: {
+    client: "sqlite3",
+    connection: {
+      filename: "./database/test.db3"
+    },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    },
+    migrations: {
+      directory: "./database/migrations",
+      tableName: "dbmigrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    }
+  }
+};
